7d5cda86222d9ca4fea580ef9b6fe073ce7f9c5d
Denys <semenyuk.denis@gmail.com>
2021-02-21 19:00:18 +0200

    added script for upload files SFTP and log to lesson4 folder

--- lesson4/sftp_copy.sh
+++ lesson4/sftp_copy.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+#settup logging
+set -e
+LOG_F="./loggin_upload_to_sftp.log"
+exec &> >(tee "${LOG_F}")
+
 #login sftp to cent_os
 sftp dseme@192.168.88.2 <<EOF
 
 mkdir FTP_ch3
 
 put -r FTP_ch3
-
+put -r ./loggin_upload* ./devops_crash/lesson4 
+##################
+# Uploaded files #
+##################
+ls -l FTP_ch3
 exit
 EOF
+
