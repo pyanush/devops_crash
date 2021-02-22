febc05b9fc60ba738b147f6979c5deebebc78a35
devTarik <tarik05@ukr.net>
2021-02-22 18:21:10 -0500

    add README

---
+++ lesson3/Create_new_user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+LOG_F="Create_user_$1_"`date "+%F-%T"`".log"
+exec &> >(tee "${LOG_F}")
+echo "Logging setup to ${LOG_F}"
+
+sudo yum update -y
+adduser $1
+passwd $1
+sudo usermod -aG wheel $1
+sudo echo '$1 ALL=(ALL) NOPASSWD:ALL'>/etc/sudoers.d/$1
+sudo chmod 0440 /etc/sudoers.d/$1
+sudo mkdir /home/$1/.ssh
+sudo chown -R $1:$1 /home/$1/.ssh/
---
+++ lesson3/Create_sftp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+LOG_F="Install SFTP"`date "+%F-%T"`".log"
+exec &> >(tee "${LOG_F}")
+echo "Logging setup to ${LOG_F}"
+
+sudo yum install epel-release
+sudo yum update -y
+sudo yum install vsftpd
+sudo systemctl start vsftpd
+sudo systemctl enable vsftpd
+sudo firewall-cmd --zone=public --permanent --add-port=21/tcp
+sudo firewall-cmd --zone=public --permanent --add-service=ftp
+sudo firewall-cmd --reload
+
+sudo cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.default
+
+sed 's/anonymous_enable=YES anonymous_enable=NO' /etc/vsftpd/vsftpd.conf
+sed 's/#write_enable=YES write_enable=YES' /etc/vsftpd/vsftpd.conf
+sed 's/#chroot_local_user=YES chroot_local_user=YES' /etc/vsftpd/vsftpd.conf
+sed 's/#/(default follows) allow_writeable_chroot=YES' /etc/vsftpd/vsftpd.conf
+sed 'i/tcp_wrappers=YES userlist_deny=NO' /etc/vsftpd/vsftpd.conf
+sed 'i/userlist_deny=NO userlist_file=/etc/vsftpd/user_list' /etc/vsftpd/vsftpd.conf
+
+sudo systemctl restart vsftpd
