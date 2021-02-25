15d11158c4c0288651a20e57899c83bf112e4988
Olya_Lucky <br.adgroo.ve@gmail.com>
2021-02-25 13:45:53 +0200

    H/W: L5: Added customized Vagrantfile

--- lesson4/L4_FTP_file_upload.sh
+++ lesson4/L4_FTP_file_upload.sh
@@ -10,11 +10,27 @@ echo Please, enter the filename, you want to upload (make sure to specify its fu
 read file_name
 echo So, your file is $file_name
 
-# 3. Actual file uploading process
-echo Let's upload your file to the server. Please, give "admin" password
-read admin_pass
-echo Here we go!Your file will be saved at FTP server -> Documents folder
-# Sample is: scp "Trafoier_feat_Sophiella__Take_Me_Home.mp3" admin@192.168.43.39:Documents
-scp $file_name admin@192.168.43.39:Documents
+# 3.
+echo And now specify source host as well as destination host and its folder
+echo source_host
+read source_host
+#echo source_folder
+#read source_folder
+echo destination_host
+read destination_host
+echo destination_folder
+read destination_folder
 
-# 4. TBD - Add-up verification if the file was acutaly successfully uploaded to the destination
+# 4. Actual file uploading process
+echo Let\'s upload your file to the server. Please, give "admin" password
+
+# Sample: scp "Trafoier_feat_Sophiella__Take_Me_Home.mp3" admin@192.168.43.39:Documents
+
+scp admin@$source_host/$file_name admin@$destination_host:$destination_folder
+
+# 5.
+echo YAHOO! Your file has been saved at FTP server. You may find it at $destination_host:$destination_folder
+
+TBD - Add-up verification if the file was acutaly successfully uploaded to the destination
+
+#   PWD: *,<R#!$(2udw{Zgz
