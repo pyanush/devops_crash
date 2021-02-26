aed6f8a11d0ae0ea1edbaa04febccf0e261c2377
PolyakPavlo <78595276+PolyakPavlo@users.noreply.github.com>
2021-02-26 15:53:57 +0200

    Delete ls6_2.py

--- ls6_2.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('Vagrantfile') as file:
-        for i in file.read().split('\n'):
-          with open('Vagrantfile_', 'a') as file:
-                  i = re.sub(r'(\#) (.+)', '', i)
-                  if (len (i) > 3):
-                        file.write(i+'\n')
-                        print(i)
