934e30c3cdb3d4d518d11c78bc5270635018a462
PolyakPavlo <78595276+PolyakPavlo@users.noreply.github.com>
2021-03-10 12:55:14 +0200

    fixed sql_core

--- lesson8/sql_orm.py
+++ lesson8/sql_orm.py
@@ -1,8 +1,5 @@
-import os
-import sys
-import datetime
-import pandas as pd
 import re
+import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String, MetaData, Table, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -14,7 +11,6 @@ logs_entries = []
 access_logs =[]
 meta = MetaData()
 engine = create_engine('sqlite:///access_logs.db', echo = True)
-
 Base = declarative_base()
 
 
@@ -24,7 +20,7 @@ class LogEntry(Base):
     hostname = Column('host', String)
     ip_address = Column('ip', String)
     date_time = Column('date', DateTime)
-    message = Column('desc', String)
+    message = Column('description', String)
     def __init__(self, **kwargs): self.__dict__.update(kwargs)
     # def __repr__(self): return "(id='%s', date='%s', ip='%s', msg='%s', hostname='%s')" % (self.id, self.date, self.ip,  self.msg, self.hostname)
 
@@ -38,28 +34,13 @@ except: file = open('logfile.log', 'r')
 
 for lines in file:
     line = re.compile(r'^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(\S+)\s+(sshd)\S+:\s+(.*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*)$').search(lines)
-    e=re.findall(r'Failed.+',lines)
-    
     if line:
-        if e: err.append({"ip_address": line.group(6), "error": e})    
         datetime_str = line.group(1) + " " +str(datetime.datetime.now().year)
         datetime_obj = datetime.datetime.strptime(datetime_str,'%b %d %H:%M:%S %Y')
-        logs_entries.append({"hostname": line.group(3),"ip_address": line.group(6), "date_time": datetime_obj,"message": line.group(5)})
         log = LogEntry(hostname=line.group(3), ip_address=line.group(6),date_time=datetime_obj, message = line.group(5))
-        session.add(log)
-session.commit()
+        logs_entries.append(log)
 file.close()
-logs_entries = []
 session.bulk_save_objects(logs_entries)
+session.add(log)
 session.commit()
 session.close()
-
-# df = pd.DataFrame(logs_entries)
-# # df["hostname"].mask(df["hostname"].duplicated(),inplace=True)
-# # df["ip_address"].mask(df["ip_address"].duplicated(),inplace=True)
-# df.sort_values(by=['ip_address'], inplace=True)
-# df.to_excel("access_logs.xlsx")
-df = pd.DataFrame(err)
-df.sort_values(by=['ip_address'], inplace=True)
-df.to_excel("error_logs.xlsx")
-os.system('sqlitebrowser access_logs.db')
