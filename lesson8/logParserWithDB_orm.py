8fa001bfd45345472ebe134f014d11bdad2dffd9
@nikolasht1 <nikolasht@outlook.com>
2021-03-15 01:03:32 +0200

    fix eror line48

--- lesson8/logParserWithDB_orm.py
+++ lesson8/logParserWithDB_orm.py
@@ -1,6 +1,15 @@
 import datetime
 import re
 import pandas as pd
+from os import access
+from sqlalchemy import create_engine, engine, log
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import session
+from sqlalchemy.orm.session import Session
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import DateTime, Integer, String
+from sqlalchemy.orm import sessionmaker
+
 
 inputFile = open ("lesson7/logfile.log", 'r',encoding='utf-8')
 
@@ -16,4 +25,30 @@ for entry in inputFile:
         log_entries.append ({"hostname": line.group (3), "ip_address": line.group (6), "date_time": datetime_obj,"message": line.group (5)})
 
 inputFile.close ()
+print(type(log_entries))
 
+'''
+engine = create_engine('sqlite:///lesson8/access_logs.db', echo = True)
+Base = declarative_base()
+
+class LogEntry(Base):
+    __tablename__ = 'access_logs'
+
+    id = Column(Integer, primary_key=True)
+    hostname = Column(String)
+    ip_address = Column(String)
+    date_time = Column(DateTime)
+    message = Column(String)
+Base.metadata.create_all(engine)  
+
+Session = sessionmaker(bind = engine)
+session = Session()  
+log = LogEntry(hostname = 'hostname', ip_address = 'ip address', date_time = datetime_obj, message = 'who is')
+session.add(log)
+
+#logs_entries = log_entries
+session.add_all([log_entries])
+session.commit()
+
+session.close()
+'''
