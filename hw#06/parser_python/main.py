3eb4a75749aacee864d4364a1ea90b256144354c
A.Tymchuk <ndrx@uarnet.cv.ua>
2021-03-17 19:04:01 +0200

    v.0.2

--- hw#06/parser_python/main.py
+++ hw#06/parser_python/main.py
@@ -5,23 +5,31 @@ import re
 
 def run(file):
     print(f'FILE: {file}')
-    months = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
-    month = re.findall(r"\bFeb\b", STR)
-    print(f"MONTH: {month}")
-
-
-    # file = open(file=file, mode='r', encoding='utf-8')
-    # number_str = 0
-    # for line in file:
-    #     print(line, end="")
-    #     month = re.findall(r"\bFeb\b", line)
-    #     print(f"MONTH: {month}")
-    #     number_str += 1
-    # print(f"\nnumber_str = {number_str}")
-    # file.close()
+    # months = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+    file = open(file=file, mode='r', encoding='utf-8')
+    number_str = 0
+    for line in file:
+        # print(line, end="")
+        # part1 = re.findall(r"^\w{3}\s\d{2}\s[0-9]{2}\:[0-9]{2}\:[0-9]{2}\s\w+\s[a-zA-Z-]+\[?\w+\]?\:", line)
+        # print(f"part1: {part1}")
+        # part2 = re.findall(r"^\w{3}\s\w+\s[0-9:]+\s\w+\s[a-zA-Z-]+\[?\w+\]?\:.+$", line)
+        part2 = re.findall(r"\s[a-zA-Z-]+\[?\w+\]?\:.+$", line)
+        print(f"part2: {part2}")
+        # month = re.findall(r"^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", line)
+        # day = re.findall(r"\s[0-9]{2}\s", line)
+        # time = re.findall(r"[0-9]{2}\:[0-9]{2}\:[0-9]{2}", line)
+        # host = re.findall(r"(?<=\:[0-9]{2}\s)[a-zA-Z]+[0-9]+", line)
+        # service = re.findall(r"[-a-z]+\[[0-9]+\]", line)
+        # ip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", line)
+        # # msg = re.findall(r"(?<=\]\:)\[\w*\]", line)
+        # print(f"->MONTH: {month}, DAY: {day}, TIME: {time}, HOSTNAME: {host}, SERVICE: {service}, IP: {ip}")
+        # # print(f"MSG: {msg}")
+        number_str += 1
+    print(f"\nnumber_str = {number_str}")
+    file.close()
 
 if __name__ == '__main__':
-    # run('logfile/test.log')
-    STR = "Feb 15 13:01:58 vmi526857 sshd[566]: error: maximum authentication attempts exceeded for root from 218.92.0.172 port 39883 ssh2 [preauth]"
-    run(STR)
+    run('logfile/test.log')
+    # STR = "Feb 15 13:01:58 vmi526857 sshd[566]: error: maximum authentication attempts exceeded for root from 218.92.0.172 port 39883 ssh2 [preauth]\n"
+    # run(STR)
 
