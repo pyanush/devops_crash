20c9383c2b22f4dd93a7dc5bb76aa82cdb6f8b46
A.Tymchuk <ndrx@uarnet.cv.ua>
2021-03-18 17:14:27 +0200

    Python parser script:v.1.0

--- hw#06/parser_python/main.py
+++ hw#06/parser_python/main.py
@@ -1,35 +1,32 @@
 import re
-#Feb 15 13:01:58 vmi526857 sshd[566]: error: maximum authentication attempts exceeded for root from 218.92.0.172 port 39883 ssh2 [preauth]
-
-
 
 def run(file):
     print(f'FILE: {file}')
-    # months = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
     file = open(file=file, mode='r', encoding='utf-8')
     number_str = 0
     for line in file:
-        # print(line, end="")
-        # part1 = re.findall(r"^\w{3}\s\d{2}\s[0-9]{2}\:[0-9]{2}\:[0-9]{2}\s\w+\s[a-zA-Z-]+\[?\w+\]?\:", line)
-        # print(f"part1: {part1}")
-        # part2 = re.findall(r"^\w{3}\s\w+\s[0-9:]+\s\w+\s[a-zA-Z-]+\[?\w+\]?\:.+$", line)
-        part2 = re.findall(r"\s[a-zA-Z-]+\[?\w+\]?\:.+$", line)
-        print(f"part2: {part2}")
-        # month = re.findall(r"^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)", line)
-        # day = re.findall(r"\s[0-9]{2}\s", line)
-        # time = re.findall(r"[0-9]{2}\:[0-9]{2}\:[0-9]{2}", line)
-        # host = re.findall(r"(?<=\:[0-9]{2}\s)[a-zA-Z]+[0-9]+", line)
-        # service = re.findall(r"[-a-z]+\[[0-9]+\]", line)
-        # ip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", line)
-        # # msg = re.findall(r"(?<=\]\:)\[\w*\]", line)
-        # print(f"->MONTH: {month}, DAY: {day}, TIME: {time}, HOSTNAME: {host}, SERVICE: {service}, IP: {ip}")
-        # # print(f"MSG: {msg}")
         number_str += 1
-    print(f"\nnumber_str = {number_str}")
+        print(f"str[{number_str}]-> {line}", end="")
+        split = re.findall(r"(\w{3}\s\d{2}\s[0-9]{2}\:[0-9]{2}\:[0-9]{2}\s\w+\s[a-zA-Z-]+\[?\w+?\]?)\:\s(.+$)", line)
+        part = split[0]
+        # print(f"part: {part}")
+        # print(f"part0: {part[0]}")
+        # print(f"part1: {part[1]}")
+        split1 = re.findall(r"^(\w{3})\s([0-9]{2})\s([0-9]{2}\:[0-9]{2}\:[0-9]{2})\s([a-zA-Z]+[0-9]+)\s([a-zA-Z-].+)", part[0])
+        part1_1 = split1[0]
+        # print(f"split1: {part1_1}")
+        month = part1_1[0]
+        day = part1_1[1]
+        time = part1_1[2]
+        host = part1_1[3]
+        service = part1_1[4]
+        ip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", part[1])
+        msg = part[1]
+        print(f"MONTH: {month}, DAY: {day}, TIME: {time}, HOSTNAME: {host}, SERVICE: {service}, IP: {ip}")
+        print(f"MSG: {msg}")
+        print("----------------------------------------------------------------------------")
     file.close()
 
 if __name__ == '__main__':
     run('logfile/test.log')
-    # STR = "Feb 15 13:01:58 vmi526857 sshd[566]: error: maximum authentication attempts exceeded for root from 218.92.0.172 port 39883 ssh2 [preauth]\n"
-    # run(STR)
 
