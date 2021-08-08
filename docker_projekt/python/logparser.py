a4b08c91ccaad1d41d198c30f8861c7f34a467d3
Mykola Shtefanovych <75587131+nikolasht1@users.noreply.github.com>
2021-08-09 00:49:23 +0300

    09.08.2021

--- docker_projekt/python/logparser.py
+++ docker_projekt/python/logparser.py
@@ -1,26 +1,32 @@
 import datetime
 import re
+
+import numpy as np
 import pandas as pd
 
-inputFile = open ("/app/logfile.log", 'r',encoding='utf-8')
+inputFile = open("/app/logfile.log", 'r', encoding='utf-8')
 
 log_entries = []
 
 for entry in inputFile:
-    line = re.compile (
-        r'^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(\S+)\s+(sshd)\S+:\s+(.*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*)$').search (
-        entry)
+    line = re.compile(
+        r'^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+(\S+)\s+('
+        r'sshd)\S+:\s+(.*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*)$').search(entry)
     if line:
-        datetime_str = line.group (1) + " " + str (datetime.datetime.now ().year)
-        datetime_obj = datetime.datetime.strptime (datetime_str, '%b %d %H:%M:%S %Y')
-        log_entries.append ({"hostname": line.group (3), "ip_address": line.group (6), "date_time": datetime_obj,"message": line.group (5)})
+        datetime_str = line.group(1) + " " + str(datetime.datetime.now().year)
+        datetime_obj = datetime.datetime.strptime(datetime_str, '%b %d %H:%M:%S %Y')
+        log_entries.append({"hostname": line.group(3), "ip_address": line.group(6), "date_time": datetime_obj, "message": line.group(5)})
 
-inputFile.close ()
+inputFile.close()
 
 # Excel file:
-df = pd.DataFrame (log_entries)
+df = pd.DataFrame(log_entries)
 df["hostname"].mask(df["hostname"].duplicated(), inplace=True)
 df["ip_address"].mask(df["ip_address"].duplicated(), inplace=True)
-df.to_excel("/upload/formatt.xlsx")
-while True:
-    pass
+len(df)
+part_1_df, part_2_df = np.split(df, [int(len(df) * .50)])
+part_1_df.to_excel('/app/upload/formated1_log.xlsx')
+part_2_df.to_excel('/app/upload/formated2_log.xlsx')
+
+#while True:
+ #   pass
